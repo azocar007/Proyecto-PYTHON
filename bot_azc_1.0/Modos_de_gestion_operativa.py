9601b4dbd4b639673486b878d950eb9a0130b46d
azocar007 <166507820+azocar007@users.noreply.github.com>
2025-03-08 15:31:12 +0000

    08/03/2025

--- bot_azc_1.0/Modos_de_gestion_operativa.py
+++ bot_azc_1.0/Modos_de_gestion_operativa.py
@@ -218,7 +218,7 @@ def cal_snow_ball(datos_calculados: dict):
 
 # Clase para la gestión de posiciones LONG
 class PosicionLong:
-    # Variables de la clase 
+    # Variables de la clase
     def __init__(self, entrada_de_datos: dict):
         # Variables del diccionario de entrada de datos
         self.gestion_seleccionada = entrada_de_datos["gestion_seleccionada"] # UNIDIRECCIONAL SHORT LONG - DOBLE TAP - SNOW BALL - RATIO BENEFICIO/PERDIDA
@@ -237,7 +237,7 @@ class PosicionLong:
         self.cantidad_de_reentradas = entrada_de_datos["cantidad_de_reentradas"]
         self.cantidad_decimales_monedas = entrada_de_datos["cantidad_decimales_monedas"] # Cantidad de decimales en las monedas
         self.cantidad_decimales_precio = entrada_de_datos["cantidad_decimales_precio"] # Cantidad de decimales en los precios
-        self.valor_pips = entrada_de_datos["valor_pips"] 
+        self.valor_pips = entrada_de_datos["valor_pips"]
         self.gestion_take_profit = entrada_de_datos["gestion_take_profit"] # "% TAKE PROFIT" - "LCD (Carga y Descarga)"
         self.ratio = entrada_de_datos["ratio"]
 
@@ -245,7 +245,7 @@ class PosicionLong:
     def recompras(self):
         # Definir las variables de la función con las claves del diccionario    
         i = 0
-        modo_gest = self.gestion_seleccionada # "UNIDIRECCIONAL LONG"
+        modo_gest = "UNIDIRECCIONAL LONG"
         precio = self.entrada_long
         monedas = self.cantidad_monedas_long
         monto_sl = self.monto_de_sl
@@ -319,17 +319,27 @@ class PosicionLong:
     def stop_loss(self):
         """
         Se deben redefinir las variables: 
-        posicion_actual
+        posicion_actual = COLOCAR LA FUNCIÓN QUE LLAMA LA POSICIÓN ACTUAL DE LA OPERACIÓN EN EL ACTIVO
         monto_de_sl = self.monto_de_sl
         cantidad_monedas_actual
         cantidad_decimales_precio
         """
         precio_sl = round((self.entrada_long - self.monto_de_sl / self.cantidad_monedas_long), self.cantidad_decimales_precio)
-        """
-        return {"Precio de entrada" : self.entrada_long,
-                "Volumen moneda total": self.cantidad_monedas_long,
+        return {"Volumen moneda total": self.cantidad_monedas_long,
                 "Precio de stop loss": precio_sl}
+
+    def take_profit(self):
         """
+        Se deben redefinir las variables: 
+        posicion_actual = COLOCAR LA FUNCIÓN QUE LLAMA LA POSICIÓN ACTUAL DE LA OPERACIÓN EN EL ACTIVO
+        monto_de_sl = self.monto_de_sl
+        cantidad_monedas_actual
+        cantidad_decimales_precio
+        """
+        if self.gestion_take_profit != "LCD (Carga y Descarga)":
+            precio_tp = round((self.entrada_long * self.ratio/100 + self.entrada_long), self.cantidad_decimales_precio)
+            return {"Volumen moneda total": self.cantidad_monedas_long,
+                    "Precio de take profit": precio_tp}
 
     # Funcion para calcular el volumen de las monedas
     def vol_monedas(self):
@@ -339,39 +349,109 @@ class PosicionLong:
                 "Volumen monedas total": self.cantidad_monedas_long,
                 "Precio de stop loss": self.entrada_stoploss}
 
-""" ESTA SECUENCIA DE CODIGO  DEBE EMPLEAR PARA CALCULAR LA CANTIDAD DE DECIMALES EN LAS MONEDAS Y LOS PRECIOS
-        if modo_seleccion_volumen == "USDT":
-            if (gestion_seleccionada == "UNIDIRECCIONAL SHORT" or gestion_seleccionada == "RATIO BENEFICIO/PERDIDA SHORT"):
-                cantidad_monedas_short = round(float(monto_de_sl) / entrada_short, cant_decimales_sl)
-                cantidad_monedas = cantidad_monedas_short
-                cantidad_usdt_short = round(cantidad_monedas_short * entrada_short, 2)
-                cantidad_usdt_long = "N/A"
-                cantidad_monedas_long = "N/A"
-            elif (gestion_seleccionada == "UNIDIRECCIONAL LONG" or gestion_seleccionada == "RATIO BENEFICIO/PERDIDA LONG"):
-                cantidad_monedas_long = round(float(monto_de_sl) / entrada_long, cant_decimales_sl)
-                cantidad_monedas = cantidad_monedas_long
-                cantidad_usdt_long = round(cantidad_monedas_long * entrada_long, 2)
-                cantidad_usdt_short = "N/A"
-                cantidad_monedas_short = "N/A"
+# Clase para la gestión de posiciones SHORT
+class PosicionShort:
+    # Variables de la clase 
+    def __init__(self, entrada_de_datos: dict):
+        # Variables del diccionario de entrada de datos
+        self.gestion_seleccionada = entrada_de_datos["gestion_seleccionada"] # UNIDIRECCIONAL SHORT LONG - DOBLE TAP - SNOW BALL - RATIO BENEFICIO/PERDIDA
+        self.gestion_de_entrada = entrada_de_datos["gestion_de_entrada"] # MERCADO - LIMITE - BBO
+        self.entrada_long = entrada_de_datos["entrada_long"]
+        self.entrada_short = entrada_de_datos["entrada_short"]
+        self.porcentaje_dist_reentradas = entrada_de_datos["porcentaje_dist_reentradas"]
+        self.cantidad_usdt_long = entrada_de_datos["cantidad_usdt_long"]
+        self.cantidad_usdt_short = entrada_de_datos["cantidad_usdt_short"]
+        self.cantidad_monedas_long = entrada_de_datos["cantidad_monedas_long"]
+        self.cantidad_monedas_short = entrada_de_datos["cantidad_monedas_short"]
+        self.modo_seleccionado = entrada_de_datos["modo_seleccionado"] # % DE REENTRADAS - MARTINGALA - AGRESIVO
+        self.porcentaje_vol_reentrada = entrada_de_datos["porcentaje_vol_reentrada"]
+        self.monto_de_sl = entrada_de_datos["monto_de_sl"]
+        self.entrada_stoploss = entrada_de_datos["entrada_stoploss"]
+        self.cantidad_de_reentradas = entrada_de_datos["cantidad_de_reentradas"]
+        self.cantidad_decimales_monedas = entrada_de_datos["cantidad_decimales_monedas"] # Cantidad de decimales en las monedas
+        self.cantidad_decimales_precio = entrada_de_datos["cantidad_decimales_precio"] # Cantidad de decimales en los precios
+        self.valor_pips = entrada_de_datos["valor_pips"]
+        self.gestion_take_profit = entrada_de_datos["gestion_take_profit"] # "% TAKE PROFIT" - "LCD (Carga y Descarga)"
+        self.ratio = entrada_de_datos["ratio"]
+
+    # Metodo de recompras
+    def recompras(self):
+        # Definir las variables de la función con las claves del diccionario    
+        i = 0
+        modo_gest = "UNIDIRECCIONAL SHORT"
+        precio = self.entrada_short
+        monedas = self.cantidad_monedas_short
+        monto_sl = self.monto_de_sl
+        decimales_pre = self.cantidad_decimales_precio
+        cant_ree = self.cantidad_de_reentradas
+        porcentaje_ree = self.porcentaje_dist_reentradas
+        gestion_volumen = self.modo_seleccionado
+        porcentaje_vol = self.porcentaje_vol_reentrada
+        decimales_mon = self.cantidad_decimales_monedas
+
+        # Definiendo el valor N/A de monedas
+        if monedas == "N/A":
+            monedas = round(self.cantidad_usdt_short / precio, decimales_mon)
+
+        # Definiendo valores iniciales de las listas
+        list_reentradas = [precio]
+        vol_monedas = [monedas]
+        vol_usdt = [round(precio*monedas,4)]
+        precios_prom = []
+        precios_stop_loss = []
+        precio_sl = round((precio + monto_sl / monedas), decimales_pre)
+
+        # Condicional para corregir el valor de "cero 0" en la cantidad de reentradas
+        if cant_ree <= 0:
+            cant_ree = 1000
+
+        # Bucle para obtener las listas
+        while i < cant_ree and precio_sl > precio:
+            # Iterador
+            i += 1
+            # Reentradas:
+            precio = round((precio - (precio * porcentaje_ree/100)), decimales_pre)
+            # vol_monedas:
+            if gestion_volumen == "MARTINGALA":
+                monedas = gest_martingala(vol_monedas, porcentaje_vol, decimales_mon)
+            elif gestion_volumen == "% DE REENTRADAS":
+                monedas = gest_porcen_reentradas(monedas, porcentaje_vol, decimales_mon)
             else:
-                cantidad_monedas_long = round(float(cantidad_monedas1) / entrada_long, cantidad_decimales_monedas)
-                cantidad_monedas = cantidad_monedas_short = cantidad_monedas_long
-                cantidad_usdt_long = round(cantidad_monedas_long * entrada_long, 2)
-                cantidad_usdt_short = round(cantidad_monedas_short * entrada_short, 2)
-        else:  # modo_seleccion_volumen == "MONEDAS":
-            cantidad_monedas = cantidad_monedas_long = cantidad_monedas_short = cantidad_monedas1
-            if gestion_seleccionada == "UNIDIRECCIONAL SHORT":
-                cantidad_usdt_short = round(float(cantidad_monedas) * entrada_short, 2)
-                cantidad_monedas_long = "N/A"
-                cantidad_usdt_long = "N/A"
-            elif gestion_seleccionada == "UNIDIRECCIONAL LONG":
-                cantidad_usdt_long = round(float(cantidad_monedas) * entrada_long, 2)
-                cantidad_monedas_short = "N/A"
-                cantidad_usdt_short = "N/A"
-            else:
-                cantidad_usdt_long = round(float(cantidad_monedas) * entrada_long, 2)
-                cantidad_usdt_short = round(float(cantidad_monedas) * entrada_short, 2)
-"""
+                monedas = gest_agresivo(precio, porcentaje_vol, vol_monedas, vol_usdt, decimales_mon, modo_gest)
+            # Precios_prom (precios promedios)
+            usdt = round(monedas * precio, 4)
+            prom = round(sum(vol_usdt) / sum(vol_monedas), decimales_pre)
+            # Precio de Stop Loss
+            precio_sl = round(prom + monto_sl / sum(vol_monedas),decimales_pre)
+            # Ingreso de resultados a las listas correspondientes
+            vol_usdt.append(usdt)
+            vol_monedas.append(monedas)
+            list_reentradas.append(precio)
+            precios_prom.append(prom)
+            precios_stop_loss.append(precio_sl)
+        # Eliminando elementos que sobran en las listas
+        vol_monedas.pop()
+        list_reentradas.pop()
+        vol_acum = round(sum(vol_monedas),decimales_mon)
+        vol_usdt_total = round(vol_acum * precios_prom[-1], self.cantidad_decimales_monedas)
+        if cant_ree > len(list_reentradas):
+            mensj = "Cantidad de entradas solicitadas es mayor a las calculadas."
+        else:
+            mensj = "Cantidad de entradas acorde a lo establecido"
+        # Retorno de resultados
+        return {"Precios de reentradas": list_reentradas,
+                "Precios promedios": precios_prom,
+                "Precios de stop loss": precios_stop_loss,
+                "Precio de stop loss": precios_stop_loss[-1],
+                "Volumenes de monedas": vol_monedas,
+                "Volumen monedas total": vol_acum,
+                "Volumen USDT total": vol_usdt_total,
+                "Mensaje": mensj}
+
+    # Metodo de stop loss
+    def stop_loss(self):
+        pass
+
 
 
 
@@ -383,82 +463,34 @@ datos_de_entrada = {
             "entrada_long": 0.2589,
             "entrada_short": 0.2574,
             "porcentaje_dist_reentradas": 2,
-            "cantidad_usdt_long" : 10.0,
-            "cantidad_usdt_short" : 10.0,
+            "cantidad_usdt_long" : 10,
+            "cantidad_usdt_short" : 10,
             "cantidad_monedas_long": 39,
             "cantidad_monedas_short": 39,
             "modo_seleccionado": "% DE REENTRADAS", # % DE REENTRADAS - MARTINGALA - AGRESIVO
             "porcentaje_vol_reentrada": 50,
             "monto_de_sl": 10.0,
             "entrada_stoploss": 0.2700,
-            "cantidad_de_reentradas": 5,
-            "cantidad_decimales_monedas": 2,
+            "cantidad_de_reentradas": 10,
+            "cantidad_decimales_monedas": 0,
             "cantidad_decimales_precio": 4,
             "valor_pips": 0.0001,
             "gestion_take_profit": "RATIO BENEFICIO/PERDIDA", # "% TAKE PROFIT" - "LCD (Carga y Descarga)"
-            "ratio": 2
+            "ratio": 4
             }
 #"""
 
-# Empleando la función entrada_de_datos
-#datos_calculados = entrada_de_datos()
-"""
-if datos_calculados["gestion_seleccionada"] == "DOBLE TAP":
+# Comprobando el modulo
 
-    # Gestion SHORT:
-    print("\nDATOS DE GESTION DOBLE TAP:\n")
-    lista_reentradas_short, lista_vol_mon_short, vol_mon_total_short, lista_prom_short, lista_stoploss_short, mensaje, volum_usdt_total = cal_unidereccional_short(datos_a_calc)
-    print(f""\nDATOS DE GESTION SHORT:\n
-        Las entradas son: {lista_reentradas_short}
-        Los volumenes son:{lista_vol_mon_short}
-        El volumen acumulado es: {vol_mon_total_short} Monedas => {volum_usdt_total} USDT
-        Los ptos promedios son: {lista_prom_short}
-        Los precios de Stop Loss son:{lista_stoploss_short}
-        {mensaje}\n"")
-    # Gestion LONG:
-    lista_reentradas_long, lista_vol_mon_long, vol_mon_total_long, lista_prom_long, lista_stoploss_long, mensaje, volum_usdt_total = cal_unidereccional_long(datos_a_calc)
-    print(f""\nDATOS DE GESTION LONG:\n
-        Las entradas son: {lista_reentradas_long}
-        Los volumenes son:{lista_vol_mon_long}
-        El volumen acumulado es: {vol_mon_total_long} Monedas => {volum_usdt_total} USDT
-        Los ptos promedios son: {lista_prom_long}
-        Los precios de Stop Loss son:{lista_stoploss_long}
-        {mensaje}\n"")
-    pass
+#Datos_calculados = PosicionLong(entrada_de_datos())
+Datos_calculados_long= PosicionLong(datos_de_entrada)
+Datos_calculados_short= PosicionShort(datos_de_entrada)
 
-elif datos_calculados["gestion_seleccionada"] == "UNIDIRECCIONAL LONG":
-    #pass
-    lista_reentradas_long, lista_vol_mon_long, vol_mon_total_long, lista_prom_long, lista_stoploss_long, mensaje, volum_usdt_total = cal_unidereccional_long(datos_a_calc)
-    print(f""\nDATOS DE GESTION UNIDIRECCIONAL LONG:
-        Las entradas son: {lista_reentradas_long}
-        Los volumenes son:{lista_vol_mon_long}
-        El volumen acumulado es: {vol_mon_total_long} Monedas => {volum_usdt_total} USDT
-        Los ptos promedios son: {lista_prom_long}
-        Los precios de Stop Loss son:{lista_stoploss_long}
-        {mensaje}\n"")
-    pass
+#pprint.pprint(Datos_calculados_long.recompras())
+#pprint.pprint(Datos_calculados_long.vol_monedas())
+#pprint.pprint(Datos_calculados_long.take_profit())
 
-elif datos_calculados["gestion_seleccionada"] == "UNIDIRECCIONAL SHORT":
-    #pass
-    lista_reentradas_short, lista_vol_mon_short, vol_mon_total_short, lista_prom_short, lista_stoploss_short, mensaje, volum_usdt_total = cal_unidereccional_short(datos_a_calc)
-    print(f""\nDATOS DE GESTION UNIDIRECCIONAL SHORT:
-        Las entradas son: {lista_reentradas_short}
-        Los volumenes son:{lista_vol_mon_short}
-        El volumen acumulado es: {vol_mon_total_short} Monedas => {volum_usdt_total} USDT
-        Los ptos promedios son: {lista_prom_short}
-        Los precios de Stop Loss son:{lista_stoploss_short}
-        {mensaje}\n"")
-    pass
-
-else: # datos_calculados[modo_gestion] == "SNOW BALL"
-    #pass
-    lista_reentradas_long, lista_reentradas_short, lista_vol_monendas, vol_mon_total, lista_prom_long, lista_prom_short, lista_stoploss_long, lista_stoploss_short = cal_snow_ball(datos_a_calc)
-    print(f"\nDATOS DE GESTION SNOW BALL:\n\nReentradas LONG: {lista_reentradas_long}\nPuntos promedios LONG: {lista_prom_long}\nPuntos de Stop Loss LONG: {lista_stoploss_long}\n\nLos volumenes de monedas son: {lista_vol_monendas},\nVolumen total acumulado: {vol_mon_total}\n\nPuntos de Stop Loss SHORT: {lista_stoploss_short}\nPuntos promedios SHORT: {lista_prom_short}\nReentradas SHORT: {lista_reentradas_short}\n")
-    pass
-"""
-
-Datos_calculados = PosicionLong(entrada_de_datos())
-#pprint.pprint(Datos_calculados.recompras())
-pprint.pprint(Datos_calculados.vol_monedas())
-
+pprint.pprint(Datos_calculados_short.recompras())
+#pprint.pprint(Datos_calculados_short.vol_monedas())
+#pprint.pprint(Datos_calculados_short.take_profit())
 
--- bot_azc_1.0/ensayos_puntuales.py
+++ bot_azc_1.0/ensayos_puntuales.py
@@ -51,3 +51,38 @@ def seleccionar_opcionn(opciones: dict, mensaje: str):
 
 exchange_seleccionado = seleccionar_opcionn(opciones_de_exchanges, "Seleccione el exchange (1-6):")
 print(exchange_seleccionado)
+
+
+""" ESTA SECUENCIA DE CODIGO  DEBE EMPLEAR PARA CALCULAR LA CANTIDAD DE DECIMALES EN LAS MONEDAS Y LOS PRECIOS
+        if modo_seleccion_volumen == "USDT":
+            if (gestion_seleccionada == "UNIDIRECCIONAL SHORT" or gestion_seleccionada == "RATIO BENEFICIO/PERDIDA SHORT"):
+                cantidad_monedas_short = round(float(monto_de_sl) / entrada_short, cant_decimales_sl)
+                cantidad_monedas = cantidad_monedas_short
+                cantidad_usdt_short = round(cantidad_monedas_short * entrada_short, 2)
+                cantidad_usdt_long = "N/A"
+                cantidad_monedas_long = "N/A"
+            elif (gestion_seleccionada == "UNIDIRECCIONAL LONG" or gestion_seleccionada == "RATIO BENEFICIO/PERDIDA LONG"):
+                cantidad_monedas_long = round(float(monto_de_sl) / entrada_long, cant_decimales_sl)
+                cantidad_monedas = cantidad_monedas_long
+                cantidad_usdt_long = round(cantidad_monedas_long * entrada_long, 2)
+                cantidad_usdt_short = "N/A"
+                cantidad_monedas_short = "N/A"
+            else:
+                cantidad_monedas_long = round(float(cantidad_monedas1) / entrada_long, cantidad_decimales_monedas)
+                cantidad_monedas = cantidad_monedas_short = cantidad_monedas_long
+                cantidad_usdt_long = round(cantidad_monedas_long * entrada_long, 2)
+                cantidad_usdt_short = round(cantidad_monedas_short * entrada_short, 2)
+        else:  # modo_seleccion_volumen == "MONEDAS":
+            cantidad_monedas = cantidad_monedas_long = cantidad_monedas_short = cantidad_monedas1
+            if gestion_seleccionada == "UNIDIRECCIONAL SHORT":
+                cantidad_usdt_short = round(float(cantidad_monedas) * entrada_short, 2)
+                cantidad_monedas_long = "N/A"
+                cantidad_usdt_long = "N/A"
+            elif gestion_seleccionada == "UNIDIRECCIONAL LONG":
+                cantidad_usdt_long = round(float(cantidad_monedas) * entrada_long, 2)
+                cantidad_monedas_short = "N/A"
+                cantidad_usdt_short = "N/A"
+            else:
+                cantidad_usdt_long = round(float(cantidad_monedas) * entrada_long, 2)
+                cantidad_usdt_short = round(float(cantidad_monedas) * entrada_short, 2)
+"""
