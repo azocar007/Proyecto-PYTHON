2c678b1d83050dc93dfc871dd990a1df36bd9d07
azocar007 <166507820+azocar007@users.noreply.github.com>
2025-05-22 21:32:45 +0000

    22/05/2025

--- bot_azc_1.0/ensayos_puntuales.py
+++ bot_azc_1.0/ensayos_puntuales.py
@@ -92,51 +92,69 @@ class Long_SMA_MACD_BB(Strategy):
         """ Paso 2: Confirmar toque de la banda """
         if self.macd_crossed:
             self.ventana_restante -= 1
-        min_price = min(self.data.Low[-self.bb_period:])
-        tope = self.data.High[-1]
-        precio = self.data.Low[-1]  # se inicia desde el valor más bajo de la vela actual
-        entry_price = None
-        precios_hist = close_series.iloc[-(self.bb_period - 1):].tolist()
+            if self.data.High[-1] > bb_up_val:
+                min_price = min(self.data.Low[-self.bb_period:])
+                tope = self.data.High[-1]
+                precio = mgo.redondeo(self.data.Low[-1], self.pip_precio)  # se inicia desde el valor más bajo de la vela actual
+                entry_price = None
+                precios_hist = close_series.iloc[-(self.bb_period - 1):].tolist()
 
-            while precio <= tope:
-                serie = pd.Series(precios_hist + [precio])
-                upper, _, _ = tap.bollinger_bands(serie, self.bb_period, self.bb_std_dev)
-                if upper.iloc[-1] >= precio:
-                    entry_price = precio
-                    break
-                precio += self.pip_precio
+                """ Bucle de fuerza bruta para conseguir el precio igual o inmediatamente superior al de la banda de bollinger """
+                while precio <= tope:
 
-                if entry_price is None:
-                    return
+                    # Calcular banda de Bollinger superior para el precio iterado
+                    serie = pd.Series(precios_hist + [precio])
+                    upper, _, _ = tap.bollinger_bands(serie, self.bb_period, self.bb_std_dev)
+
+                    # Comprobación del precio iterado para cerra el bucle
+                    if upper.iloc[-1] >= precio:
+                        entry_price = mgo.redondeo(precio, self.pip_precio)
+                        break
+
+                    # Si no se cumple, incrementar el precio iterado
+                    precio += self.pip_precio
 
                 dist_pct = abs(entry_price - min_price) / entry_price * 100
                 if dist_pct < self.dist_min:
+                    self.macd_crossed = False
+                    self.ventana_restante = self.bb_period # Validar si se necesita
                     return
 
-            stop = entry_price - abs(entry_price - min_price) * (1 + self.sep_min / 100)
-            risk = abs(entry_price - stop)
-            tp = entry_price + risk * self.ratio
+                """ No parece necesario, pero se deja por el momento """
+                if entry_price is None and self.ventana_restante >= 0:
+                    return
 
-            riesgo_usd = self.equity * self.riesgo_pct
-            size = riesgo_usd / risk
 
-            size = mgo.redondeo(size, self.pip_moneda)
-            stop = mgo.redondeo(stop, self.pip_precio)
-            tp = mgo.redondeo(tp, self.pip_precio)
+                stop = entry_price - abs(entry_price - min_price) * (1 + self.sep_min / 100)
+                risk = abs(entry_price - stop)
+                tp = entry_price + risk * self.ratio
 
-            if size > 0:
-                self.logs_trades.append({
-                    'bar_index': len(self.data.Close),
-                    'entry_price': entry_price,
-                    'macd': macd_val,
-                    'macd_signal': macd_sig_val,
-                    'sma': sma_val,
-                    'bb_upper': bb_up_val,
-                    'stop': stop,
-                    'tp': tp,
-                    'size': size
-                })
-                self.buy(size=size, sl=stop, tp=tp, market=entry_price)
+                riesgo_usd = self.equity * self.riesgo_pct
+                size = riesgo_usd / risk
+
+                size = mgo.redondeo(size, self.pip_moneda)
+                stop = mgo.redondeo(stop, self.pip_precio)
+                tp = mgo.redondeo(tp, self.pip_precio)
+
+                if size > 0:
+                    self.logs_trades.append({
+                        'bar_index': len(self.data.Close),
+                        'entry_price': entry_price,
+                        'macd': macd_val,
+                        'macd_signal': macd_sig_val,
+                        'sma': sma_val,
+                        'bb_upper': bb_up_val,
+                        'stop': stop,
+                        'tp': tp,
+                        'size': size
+                    })
+                    self.buy(size=size, sl=stop, tp=tp, market=entry_price)
+
+                self.macd_crossed = False
+
+            elif self.ventana_restante <= 0:
+                self.macd_crossed = False
+
 
 
 # Ejemplo de uso
